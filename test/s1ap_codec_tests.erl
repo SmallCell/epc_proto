580f67d915d84609291f614190c8c42328229022
VKI <lib.aca55a@gmail.com>
2014-10-30 11:06:59 +0200

    add x2 codec test

--- test/s1ap_codec_tests.erl
+++ test/s1ap_codec_tests.erl
@@ -1,19 +1,20 @@
--module(epc_proto_tests).
+-module(s1ap_codec_tests).
 
 -include_lib("eunit/include/eunit.hrl").
 -include_lib("src/S1AP.hrl").
 
--define(Hex, "00 11 00 2A 00 00 04 00 3B 00 08 00 02 F8 46 00
+-compile(export_all).
+
+-define(S1InitMsgHex, "00 11 00 2A 00 00 04 00 3B 00 08 00 02 F8 46 00
               00 00 50 00 3C 40 07 02 00 65 4E 42 30 35 00 40
               00 07 00 00 19 00 02 F8 46 00 89 40 01 40").
 
 s1ap_decoder_initiatingMessage_test() ->
-    Msg = hex:hexstr_to_list(hex:trim_whitespace(?Hex)),
-    ?debugFmt("~p~n", [Msg]),
+    Msg = hex:hexstr_to_list(hex:trim_whitespace(?S1InitMsgHex)),
+    %% ?debugFmt("~p~n", [Msg]),
     {ok, Res} = 'S1AP':decode('S1AP-PDU', Msg),
-    ?debugFmt("~p~n", [Res]),
+    %% ?debugFmt("~p~n", [Res]),
     Res.
-
 
 s1ap_encode_initiatingMessage_test() ->
     Initiatingmessage =
@@ -42,10 +43,9 @@ s1ap_encode_initiatingMessage_test() ->
                                    broadcastPLMNs = [[2,248,70]]
                                    }]},
                         #'ProtocolIE-Field'{
-                           id = 'id-DefaultPagingDRX', criticality = ignore,
+                           id = ?'id-DefaultPagingDRX', criticality = ignore,
                            value = v128}]}}},
     {ok, Res} = 'S1AP':encode('S1AP-PDU', Initiatingmessage),
-    Msg = hex:hexstr_to_list(hex:trim_whitespace(?Hex)),
+    Msg = hex:hexstr_to_list(hex:trim_whitespace(?S1InitMsgHex)),
     ?assertEqual(Msg, Res),
     Res.
-
---
+++ test/x2ap_codec_tests.erl
@@ -0,0 +1,123 @@
+-module(x2ap_codec_tests).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-include_lib("src/X2AP.hrl").
+
+-compile(export_all).
+
+%% Getting hex-dump from PCAP file
+%% tshark -x -r x2-ho-canceled.pcap -R frame.number==7 | cut -d' ' -f3-17
+-define(X2InitMsgHex, "
+      20 06 00 79 00 00 03 00 15 00 08 00 02 f8
+46 00 00 06 50 00 14 00 5c 00 48 00 42 00 02 f8
+46 00 06 50 00 06 40 02 f8 46 00 4c 77 06 27 33
+00 00 00 29 40 01 20 00 03 40 02 f8 46 00 00 10
+00 01 64 06 27 00 00 00 4c 40 02 00 64 40 02 f8
+46 00 00 10 10 01 65 06 27 00 00 00 4c 40 02 00
+64 40 02 f8 46 00 00 10 20 01 66 06 27 00 00 00
+4c 40 02 00 64 00 18 00 06 00 02 f8 46 80 02 
+").
+
+-define(X2InitMsgRec,
+        {successfulOutcome,
+         #'SuccessfulOutcome'{
+            procedureCode = 6,criticality = reject,
+            value = 
+                #'X2SetupResponse'{
+                   protocolIEs = 
+                       [#'ProtocolIE-Field'{
+                           id = ?'id-GlobalENB-ID',criticality = reject,
+                           value = 
+                               #'GlobalENB-ID'{
+                                  'pLMN-Identity' = [2,248,70],
+                                  'eNB-ID' = 
+                                      {'macro-eNB-ID',[0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1,0,1]}
+                                  }},
+                        #'ProtocolIE-Field'{
+                           id = 20,criticality = reject,
+                           value = 
+                               [#'ServedCells_SEQOF'{
+                                   servedCellInfo = 
+                                       #'ServedCell-Information'{
+                                          pCI = 66,
+                                          cellId = 
+                                              #'ECGI'{
+                                                 'pLMN-Identity' = [2,248,70],
+                                                 eUTRANcellIdentifier = 
+                                                     [0,0,0,0,0,0,0,0,0,0,0,0,0,1,1,0,0,1,0,1,0,0,0,0,0,0,0,0]
+                                                 },
+                                          tAC = [0,100],
+                                          broadcastPLMNs = [[2,248,70]],
+                                          'eUTRA-Mode-Info' = 
+                                              {fDD,
+                                               #'FDD-Info'{
+                                                  'uL-EARFCN' = 19575,'dL-EARFCN' = 1575,
+                                                  'uL-Transmission-Bandwidth' = bw50,
+                                                  'dL-Transmission-Bandwidth' = bw50
+                                                  }},
+                                          'iE-Extensions' = 
+                                              [#'ProtocolExtensionField'{
+                                                  id = 41,criticality = ignore,extensionValue = <<" ">>}]},
+                                   'neighbour-Info' = 
+                                       [#'Neighbour-Information_SEQOF'{
+                                           eCGI = 
+                                               #'ECGI'{
+                                                  'pLMN-Identity' = [2,248,70],
+                                                  eUTRANcellIdentifier = 
+                                                      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,0]
+                                                  },
+                                           pCI = 356,eARFCN = 1575,
+                                           'iE-Extensions' = 
+                                               [#'ProtocolExtensionField'{
+                                                   id = 76,criticality = ignore,
+                                                   extensionValue = <<0,100>>}]},
+                                        #'Neighbour-Information_SEQOF'{
+                                           eCGI = 
+                                               #'ECGI'{
+                                                  'pLMN-Identity' = [2,248,70],
+                                                  eUTRANcellIdentifier = 
+                                                      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,0,1]
+                                                  },
+                                           pCI = 357,eARFCN = 1575,
+                                           'iE-Extensions' = 
+                                               [#'ProtocolExtensionField'{
+                                                   id = 76,criticality = ignore,
+                                                   extensionValue = <<0,100>>}]},
+                                        #'Neighbour-Information_SEQOF'{
+                                           eCGI = 
+                                               #'ECGI'{
+                                                  'pLMN-Identity' = [2,248,70],
+                                                  eUTRANcellIdentifier = 
+                                                      [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,1,0,0,0,0,0,0,1,0]
+                                                  },
+                                           pCI = 358,eARFCN = 1575,
+                                           'iE-Extensions' = 
+                                               [#'ProtocolExtensionField'{
+                                                   id = 76,criticality = ignore,
+                                                   extensionValue = <<0,100>>}]}]
+                                   }]},
+                        #'ProtocolIE-Field'{
+                           id = 24,criticality = reject,
+                           value = 
+                               [#'GU-Group-ID'{
+                                   'pLMN-Identity' = [2,248,70],
+                                   'mME-Group-ID' = [128,2]
+                                   }]}]}}}
+       ).
+
+
+x2ap_decoder_initiatingMessage_test() ->
+    Msg = hex:hexstr_to_list(hex:trim_whitespace(?X2InitMsgHex)),
+    %% ?debugFmt("~p~n", [Msg]),
+    {ok, Res} = 'X2AP':decode('X2AP-PDU', Msg),
+    %% ?debugFmt("~p~n", [Res]),
+    Res.
+
+x2ap_encode_initiatingMessage_test() ->
+    {ok, Res} = 'X2AP':encode('X2AP-PDU', ?X2InitMsgRec),
+    %% ?debugFmt(">> ~p~n", [Res]),
+    Msg = hex:hexstr_to_list(hex:trim_whitespace(?X2InitMsgHex)),
+    %% ?debugFmt(">> ~p~n", [Msg]),
+    ?assertEqual(Msg, Res),
+    Res.
